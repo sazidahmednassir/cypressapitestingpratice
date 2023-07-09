296acb0ec2d65fc3627cacbf816b67d1aa14c1c2
sazidahmednassir <sazidahmednassir@gmail.com>
2023-07-09 17:16:46 +0600

    headers and Cookies Commit

--- cypress/e2e/APITesting/HeadersAndCookies.cy.js
+++ cypress/e2e/APITesting/HeadersAndCookies.cy.js
@@ -1,8 +1,9 @@
 describe("Headers API Testing", ()=>{
 
+    let authToken=null; 
+
     before("create access Token", ()=>{
         
-        let authToken=null; 
 
         cy.request({
               method:"POST",
@@ -18,7 +19,54 @@ describe("Headers API Testing", ()=>{
                
             }
 
+        }).then((res)=>{
+            authToken=res.body.accessToken;
         })
 
+    })
+
+    before("create Orders", ()=>{
+        
+
+        cy.request({
+              method:"POST",
+              url:"https://simple-books-api.glitch.me/orders",
+              headers:
+              {
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer '+authToken,
+             },
+            body:{
+                "bookId": 1,
+                "customerName":"John"
+            }
+
+        }).then((res)=>{
+            expect(res.status).equal(201);
+            expect(res.body.created).equal(true);
+        })
+
+    })
+
+    it('Fetching the all orders', ()=>{
+
+        cy.request({
+            method:"GET",
+            url:"https://simple-books-api.glitch.me/orders",
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer '+authToken,
+
+            },
+            cookies:{
+                'cookie_name':'sazid'
+            }
+
+
+        })
+        .then((res)=>{
+            expect(res.status).equal(200);
+            expect(res.body).has.length(1);
+        })
     })
 })

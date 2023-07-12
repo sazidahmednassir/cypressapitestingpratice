1afe365a3807cebc083da7c97d81249f4c9fc4f8
sazidahmednassir <sazidahmednassir@gmail.com>
2023-07-12 11:53:38 +0600

    JSON Schema Validation

---
+++ cypress/e2e/APITesting/SchemaValidation.cy.js
@@ -0,0 +1,87 @@
+
+
+const Ajv=require('ajv');
+
+const avj= new Ajv();
+
+
+
+describe("JSON Schema Validation", ()=>{
+
+
+    it("schema validation", ()=>{
+
+        cy.request({
+            method:"GET",
+            url:"https://fakestoreapi.com/products"
+        })
+        .then((res)=>{
+
+            const schema={
+                "$schema": "http://json-schema.org/draft-07/schema#",
+                "title": "Generated schema for Root",
+                "type": "array",
+                "items": {
+                  "type": "object",
+                  "properties": {
+                    "id": {
+                      "type": "number"
+                    },
+                    "title": {
+                      "type": "string"
+                    },
+                    "price": {
+                      "type": "number"
+                    },
+                    "description": {
+                      "type": "string"
+                    },
+                    "category": {
+                      "type": "string"
+                    },
+                    "image": {
+                      "type": "string"
+                    },
+                    "rating": {
+                      "type": "object",
+                      "properties": {
+                        "rate": {
+                          "type": "number"
+                        },
+                        "count": {
+                          "type": "number"
+                        }
+                      },
+                      "required": [
+                        "rate",
+                        "count"
+                      ]
+                    }
+                  },
+                  "required": [
+                    "id",
+                    "title",
+                    "price",
+                    "description",
+                    "category",
+                    "image",
+                    "rating"
+                  ]
+                }
+              }//schema end
+
+             const validate= avj.compile(schema)
+             const result=validate(res.body)
+             expect(result).to.be.true;
+
+        })
+    })
+
+
+
+
+
+
+
+
+})
